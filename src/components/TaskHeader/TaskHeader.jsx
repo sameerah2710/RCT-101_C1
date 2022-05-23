d6a8bcbd768df4cd834345d42867d1692e28da31
Sameer Ajay Harankhedkar <95958744+sameerah2710@users.noreply.github.com>
2022-05-23 15:56:29 +0530

    revised

--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -9,11 +9,16 @@ const TaskHeader = () => {
   // NOTE: do not delete `data-cy` key value pair
   return (
     <div data-cy="task-header" className={styles.taskHeader}>
+      <div>
+        <h1>TODO LIST</h1>
+      </div>
+      <div>
       "You have"
       <b data-cy="header-remaining-task">{unCompletedTask}</b>
       "Of"
       <b data-cy="header-total-task">{totalTask}</b>
      "Task Remaining"
+      </div>
     </div>
   );
 };
