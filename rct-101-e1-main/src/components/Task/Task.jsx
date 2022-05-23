b54c52490b57702da09282b487ba6e9c87774aee
Sameer Ajay Harankhedkar <95958744+sameerah2710@users.noreply.github.com>
2022-05-23 14:34:43 +0530

    starting with evaluation

---
+++ rct-101-e1-main/src/components/Task/Task.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./task.module.css";
+import Counter from "../Counter/Counter"
+
+const Task = () => {
+  // NOTE: do not delete `data-cy` key value pair
+  return (
+    <li data-cy="task" className={styles.task}>
+      <input type="checkbox" data-cy="task-checkbox" />
+      <div data-cy="task-text"></div>
+      <Counter/>
+      <button data-cy="task-remove-button"></button>
+    </li>
+  );
+};
+
+export default Task;
